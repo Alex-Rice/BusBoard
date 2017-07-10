de6d673bbc944164a3894a042c510d90e1a0ffce
Alex Rice <alexander.rice@sjc.ox.ac.uk>
2017-07-10 10:32:08 +0100

    Moved logic to api

---
+++ BusBoard.Api/Api.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using BusBoard.ConsoleApp;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace BusBoard.Api
+{
+    public class ApiClass
+    {
+        public static string MakeApiCalls(string input)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var postCodeInfo = DownloadPostcode(input);
+            var longLat = LongLat.FromJson(postCodeInfo);
+            var busStopJson = DownloadBusStops(longLat);
+            var stopPoints = StopPoint.FromJson(busStopJson);
+            var firstTwoStopPoints = stopPoints.OrderBy(x => x.distance).Take(2).ToList();
+            return PrintAllBus(firstTwoStopPoints);
+        }
+
+        
+        private static string DownloadFile(string input)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(@"https://api.tfl.gov.uk");
+            client.Authenticator = new SimpleAuthenticator("app_id", "2960d12f", "app_key", "f75abe891c0d1aec03fd1e396b9afe40");
+
+            var request = new RestRequest();
+            request.Resource = @"StopPoint/" + input + @"/Arrivals";
+
+
+            var response = client.Execute(request);
+            return response.Content;
+
+        }
+        private static string DownloadPostcode(string input)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(@"https://api.postcodes.io");
+
+            var request = new RestRequest();
+            request.Resource = @"postcodes/" + input;
+
+
+            var response = client.Execute(request);
+            return response.Content;
+
+        }
+
+        private static string DownloadBusStops(LongLat input)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(@"https://api.tfl.gov.uk");
+            client.Authenticator = new SimpleAuthenticator("app_id", "2960d12f", "app_key", "f75abe891c0d1aec03fd1e396b9afe40");
+
+            var request = new RestRequest();
+            request.Resource = $"StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=1000&lat={input.latitude}&lon={input.longitude}";
+
+
+            var response = client.Execute(request);
+            return response.Content;
+
+        }
+
+        private static string PrintAllBus(List<StopPoint> stops)
+        {
+            return string.Join("\n", stops.Select(PrintBus));
+        }
+
+        private static string PrintBus(StopPoint stop)
+        {
+            var retString = new StringBuilder();
+            retString.AppendLine(stop.commonName);
+            var downloadedJson = DownloadFile(stop.id);
+            var busList = Bus.FromJson(downloadedJson);
+            var firstFiveBus = busList.OrderBy(x => x.expectedArrival).Take(5);
+            foreach (var bus in firstFiveBus)
+            {
+                retString.AppendLine(bus.Print());
+            }
+            return retString.ToString();
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using RestSharp;
-using RestSharp.Authenticators;
+using BusBoard.Api;
+
 
 namespace BusBoard.ConsoleApp
 {
@@ -16,86 +9,13 @@ namespace BusBoard.ConsoleApp
         static void Main(string[] args)
         {
             var input = GetInput();
-            Console.WriteLine(MakeApiCalls(input));
-        }
-
-        private static string MakeApiCalls(string input)
-        {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var postCodeInfo = DownloadPostcode(input);
-            var longLat = LongLat.FromJson(postCodeInfo);
-            var busStopJson = DownloadBusStops(longLat);
-            var stopPoints = StopPoint.FromJson(busStopJson);
-            var firstTwoStopPoints = stopPoints.OrderBy(x => x.distance).Take(2).ToList();
-            return PrintAllBus(firstTwoStopPoints);
+            Console.WriteLine(ApiClass.MakeApiCalls(input));
         }
 
         private static string GetInput()
         {
             Console.Write("Enter Postcode: ");
             return Console.ReadLine();
-        }
-
-        private static string DownloadFile(string input)
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri(@"https://api.tfl.gov.uk");
-            client.Authenticator = new SimpleAuthenticator("app_id", "2960d12f", "app_key", "f75abe891c0d1aec03fd1e396b9afe40");
-
-            var request = new RestRequest();
-            request.Resource = @"StopPoint/" + input + @"/Arrivals";
-
-
-            var response = client.Execute(request);
-            return response.Content;
-
-        }
-        private static string DownloadPostcode(string input)
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri(@"https://api.postcodes.io");
-
-            var request = new RestRequest();
-            request.Resource = @"postcodes/" + input;
-
-
-            var response = client.Execute(request);
-            return response.Content;
-
-        }
-
-        private static string DownloadBusStops(LongLat input)
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri(@"https://api.tfl.gov.uk");
-            client.Authenticator = new SimpleAuthenticator("app_id", "2960d12f", "app_key", "f75abe891c0d1aec03fd1e396b9afe40");
-
-            var request = new RestRequest();
-            request.Resource = @"StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=1000&lat=51.5539350896633&lon=-0.14475403941841";
-
-
-            var response = client.Execute(request);
-            return response.Content;
-
-        }
-
-        private static string PrintAllBus(List<StopPoint> stops)
-        {
-            return string.Join("\n", stops.Select(PrintBus));
-        }
-
-        private static string PrintBus(StopPoint stop)
-        {
-            var retString = new StringBuilder();
-            retString.AppendLine(stop.commonName);
-            var downloadedJson = DownloadFile(stop.id);
-            var busList = Bus.FromJson(downloadedJson);
-            var firstFiveBus = busList.OrderBy(x => x.expectedArrival).Take(5);
-            foreach (var bus in firstFiveBus)
-            {
-                retString.AppendLine(bus.Print());
-            }
-            return retString.ToString();
         }
     }
 }
